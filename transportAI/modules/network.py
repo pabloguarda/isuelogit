a0b1db21ebf37da995244caaaac36923929f5701
Pablo Guarda <pabloguarda@cmu.edu>
2020-02-23 00:26:03 -0500

    Setting class arquitecture

---
+++ transportAI/modules/network.py
@@ -0,0 +1,18 @@
+class Position:
+    def __init__(self,x,y):
+        '''
+        :argument
+        '''
+        self.x = x
+        self.y = y
+
+class Node:
+
+    def __init__(self, label, pos):
+        '''
+        :argument label:
+        :argument pos: position with
+        '''
+        self.label = label
+        self.xy = coordinates
+
