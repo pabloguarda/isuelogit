38a115babe9bae2e0c957f609b082e9e1b4fc0f9
Pablo <pabloguarda@cmu.edu>
2022-03-27 21:53:10 -0400

    Update config.py

--- src/isuelogit/config.py
+++ src/isuelogit/config.py
@@ -16,21 +16,21 @@ def set_main_dir(dir = None):
         os.chdir(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
         dir = os.getcwd()
 
-    print('main dir:', dir)
+    # print('main dir:', dir)
 
-    main_dir = dir
+    dirs = {'main_dir': dir}
 
     # Paths to folders or files
-    dirs = {'folder_pablo_networks': main_dir + "/input/public/networks/pablo/",
-            'folder_tntp_networks': main_dir + "/input/public/networks/github/",
+    dirs = {'folder_pablo_networks': dirs['main_dir'] + "/input/public/networks/pablo/",
+            'folder_tntp_networks': dirs['main_dir'] + "/input/public/networks/github/",
             'Colombus_network':"/Users/pablo/OneDrive/university/cmu/2-research/datasets/private/bin-networks/Columbus",
             'Fresno_network': '/Users/pablo/OneDrive/university/cmu/2-research/datasets/private/od-fresno-sac/SR41',
             'Sacramento_network': '/Users/pablo/OneDrive/university/cmu/2-research/datasets/private/od-fresno-sac/sac',
             'Fresno_Sac_networks': '/Users/pablo/OneDrive/university/cmu/2-research/datasets/private/od-fresno-sac',
 
             # Folder to read and write data
-            'input_folder': main_dir + "/input/",
-            'output_folder': main_dir + "/output/",
+            'input_folder': dirs['main_dir'] + "/input/",
+            'output_folder': dirs['main_dir'] + "/output/",
             }
 
     # Folder to read and write network data
