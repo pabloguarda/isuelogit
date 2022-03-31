f3dc39efa18b5dec0fb2a46a79f66467a3681edf
pabloguarda <pabloguarda@cmu.edu>
2022-03-31 19:42:08 -0400

    Update reader.py

--- src/isuelogit/reader.py
+++ src/isuelogit/reader.py
@@ -177,7 +177,7 @@ def read_internal_paths(network: TNetwork) -> Paths:
             paths.append(path)
 
     assert len(paths) > 0, 'Paths were not succesfully read'
-    print(str(total_paths) + ' paths were read in ' + str(np.round(time.time() - t0, 1)) + '[s]      ')
+    print(str(total_paths) + ' paths were read in ' + str(np.round(time.time() - t0, 1)) + '[s]          ')
     
     return paths
 
